4b7c769bc46b2815d3100d20affb1c0308af4bd3
Shilyaev <shilyaev.ev@novomet.ru>
2014-12-01 10:48:47 +0500

    changed: Подправлены bat-файлы (подсчёт размеров данных)

--- bin_debug.bat
+++ bin_debug.bat
@@ -1,2 +1,4 @@
 "arm-none-eabi-objcopy.exe" -O ihex ../ksu-02_main-build/qtc_STM32-debug/ksu-02_main.elf ../ksu-02_main-build/qtc_STM32-debug/image.hex
 "arm-none-eabi-objcopy.exe" -O binary ../ksu-02_main-build/qtc_STM32-debug/ksu-02_main.elf ../ksu-02_main-build/qtc_STM32-debug/image.bin
+"arm-none-eabi-size.exe" -A -t -o ../ksu-02_main-build/qtc_STM32-debug/ksu-02_main.elf
+@pause
--- bin_release.bat
+++ bin_release.bat
@@ -1,2 +1,4 @@
 "arm-none-eabi-objcopy.exe" -O ihex ../ksu-02_main-build/qtc_STM32-release/ksu-02_main.elf ../ksu-02_main-build/qtc_STM32-release/image.hex
 "arm-none-eabi-objcopy.exe" -O binary ../ksu-02_main-build/qtc_STM32-release/ksu-02_main.elf ../ksu-02_main-build/qtc_STM32-release/image.bin
+"arm-none-eabi-size.exe" -A -t -o ../ksu-02_main-build/qtc_STM32-release/ksu-02_main.elf
+@pause
