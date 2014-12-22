2397fa2c66f427bd40641c5c9414f52d7597eacb
Shilyaev <shilyaev.ev@novomet.ru>
2014-12-22 10:31:29 +0500

    added: Метод подсчёта контрольной суммы (common.h)

--- src/common/common.h
+++ src/common/common.h
@@ -1,9 +1,25 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdint.h>
+
 enum StatusType {
   StatusOk,
   StatusError,
 };
+
+#ifdef __cplusplus
+
+/*!
+ \brief Метод подсчёта контрольной суммы
+
+ \param buf  - указатель на данные
+ \param size - размер данных
+ \param crc - начальное значение контрольной суммы (по умолчанию: 0xFFFF)
+ \return uint16_t - контрольная сумма
+*/
+uint16_t crc16_ibm(uint8_t *buf, uint8_t size, uint16_t crc = 0xFFFF);
+
+#endif
 
 #endif // COMMON_H
