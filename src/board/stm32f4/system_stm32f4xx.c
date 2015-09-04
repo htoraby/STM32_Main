13f6000544040641ec7ffbb9cb4f5de69ddf9264
Shilyaev <shilyaev.ev@novomet.ru>
2015-09-04 16:51:40 +0500

    change: Начальный адрес основной программы 0x10000 (bootloader 64kb)

--- src/board/stm32f4/system_stm32f4xx.c
+++ src/board/stm32f4/system_stm32f4xx.c
@@ -99,8 +99,8 @@
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
-//перенести таблицу векторов прерываний по адресу 0х08008000
-#define VECT_TAB_OFFSET  0x8000 /*!< Vector Table base offset field.
+//перенести таблицу векторов прерываний по адресу 0х08010000
+#define VECT_TAB_OFFSET  0x10000 /*!< Vector Table base offset field.
                                    This value must be a multiple of 0x200. */
 /******************************************************************************/
 
