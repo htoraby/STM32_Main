6953d8041f35ac2f914dce1e1dfcaa178656dbc7
Shilyaev <shilyaev.ev@novomet.ru>
2016-01-20 13:54:01 +0500

    add: Параметр состояния подключения USB flash

--- src/user/device/ccs/ccs_init_parameters.cpp
+++ src/user/device/ccs/ccs_init_parameters.cpp
@@ -10466,5 +10466,17 @@ void Ccs::initParameters()
   parameters_[CCS_NETMASK_4 - CCS_BEGIN].discret                                 = 1;
   parameters_[CCS_NETMASK_4 - CCS_BEGIN].def                                     = 0.0;
   parameters_[CCS_NETMASK_4 - CCS_BEGIN].code                                    = 0;
+  // Подключение flashdisk к USB
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].id                                 = CCS_USB_CONNECTION;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].access                             = ACCESS_OPERATOR;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].operation                          = OPERATION_READ;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].physic                             = PHYSIC_NUMERIC;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].validity                           = VALIDITY_OK;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].value.float_t                      = 0.0;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].min                                = 0.0;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].max                                = 1.0;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].discret                            = 1;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].def                                = 0.0;
+  parameters_[CCS_USB_CONNECTION - CCS_BEGIN].code                               = 0;
 }
 
