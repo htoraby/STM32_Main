88884c63fdc901b7b180dec3bcdf197de17661d5
Shilyaev <shilyaev.ev@novomet.ru>
2016-03-22 13:05:14 +0500

    fix: При программном останове увеличивался счётчик остнановов по другим защитам

--- src/user/device/ccs/ccs.cpp
+++ src/user/device/ccs/ccs.cpp
@@ -1528,7 +1528,7 @@ void Ccs::calcCountersStop(float reason)
   case LastReasonStopUnderloadMotor:
     setValue(CCS_PROT_UNDERLOAD_COUNT_STOP, getValue(CCS_PROT_UNDERLOAD_COUNT_STOP) + 1);
     break;
-  case LastReasonStopOperator: case LastReasonStopRemote:
+  case LastReasonStopOperator: case LastReasonStopRemote: case LastReasonStopProgram:
     break;
   default:
     setValue(CCS_PROT_OTHER_COUNT_STOP, getValue(CCS_PROT_OTHER_COUNT_STOP) + 1);
