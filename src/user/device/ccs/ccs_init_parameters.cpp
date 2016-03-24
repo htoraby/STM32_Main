b9589d57a63d83a51552598b5548c962f974ae0c
Shilyaev <shilyaev.ev@novomet.ru>
2016-03-24 11:39:54 +0500

    fix: Не включалась защита по турбинному вращению

--- src/user/device/ccs/ccs_init_parameters.cpp
+++ src/user/device/ccs/ccs_init_parameters.cpp
@@ -2146,7 +2146,7 @@ void Ccs::initParameters()
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].validity                    = VALIDITY_OK;
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].value.float_t               = 0;
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].min                         = 0.0;
-  parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].max                         = 1.0;
+  parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].max                         = 3.0;
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].discret                     = 1;
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].def                         = 0;
   parameters_[CCS_PROT_MOTOR_ASYNC_MODE - CCS_BEGIN].code                        = 13;
