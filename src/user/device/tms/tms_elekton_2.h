9b264ce901e980e168e765e4d464db92473230d9
trofimax <trofimenko@novomet.ru>
2016-03-25 18:47:46 +0500

    add: файлы Электон ТМС-2

---
+++ src/user/device/tms/tms_elekton_2.h
@@ -0,0 +1,30 @@
+﻿/*
+ * classTmsElekton2.h
+ *
+ *  Created on: 03.07.2014
+ *      Author: trofimenko
+ */
+
+#ifndef TMS_ELEKTON_2_H_
+#define TMS_ELEKTON_2_H_
+
+#include "tms.h"
+#include "device_modbus.h"
+
+class TmsElekton2: public Tms
+{
+public:
+  TmsElekton2();
+  virtual ~TmsElekton2();
+
+  void initModbusParameters();
+  void initParameters();
+  void init();
+
+
+private:
+  ModbusParameter modbusParameters_[15];
+  DeviceModbus *dm_;
+};
+
+#endif /* TMS_ELEKTON_2_H_ */
