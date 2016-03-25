3919638756d98a7b8336237d30da5d5cf5c2f569
trofimax <trofimenko@novomet.ru>
2016-03-25 18:48:07 +0500

    add: файлы ТМС Электон-3

---
+++ src/user/device/tms/tms_elekton_3.cpp
@@ -0,0 +1,20 @@
+﻿/*
+ * classTmsElekton3.cpp
+ *
+ *  Created on: 03.07.2014
+ *      Author: trofimenko
+ */
+
+#include "tms_elekton_3.h"
+
+TmsElekton3::TmsElekton3()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TmsElekton3::~TmsElekton3()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/user/device/tms/tms_elekton_3.h
@@ -0,0 +1,20 @@
+﻿/*
+ * classTmsElekton3.h
+ *
+ *  Created on: 03.07.2014
+ *      Author: trofimenko
+ */
+
+#ifndef TMS_ELEKTON_3_H_
+#define TMS_ELEKTON_3_H_
+
+#include "tms.h"
+
+class TmsElekton3: public Tms
+{
+public:
+  TmsElekton3();
+  virtual ~TmsElekton3();
+};
+
+#endif /* TMS_ELEKTON_3_H_ */
