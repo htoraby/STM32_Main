8765202bf07c2108eb2580561d1c8dbf0e7bc9e6
trofimax <trofimenko@novomet.ru>
2016-07-04 14:22:58 +0500

    add: класс защиты от перегрева входного фильтра

---
+++ src/user/protection/other/protection_overheat_input_filter.cpp
@@ -0,0 +1,12 @@
+#include "protection_overheat_input_filter.h"
+
+ProtectionOverheatInputFilter::ProtectionOverheatInputFilter()
+{
+
+}
+
+ProtectionOverheatInputFilter::~ProtectionOverheatInputFilter()
+{
+
+}
+
---
+++ src/user/protection/other/protection_overheat_input_filter.h
@@ -0,0 +1,12 @@
+#ifndef PROTECTIONOVERHEATINPUTFILTER_H
+#define PROTECTIONOVERHEATINPUTFILTER_H
+
+
+class ProtectionOverheatInputFilter
+{
+public:
+  ProtectionOverheatInputFilter();
+  ~ProtectionOverheatInputFilter();
+};
+
+#endif // PROTECTIONOVERHEATINPUTFILTER_H
