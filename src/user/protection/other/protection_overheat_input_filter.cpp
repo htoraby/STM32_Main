70fa581fddb725fd1c4fbb414818234a0f9e4da3
trofimax <trofimenko@novomet.ru>
2016-07-05 10:42:30 +0500

    add: защиту по перегреву входного фильтра

--- src/user/protection/other/protection_overheat_input_filter.cpp
+++ src/user/protection/other/protection_overheat_input_filter.cpp
@@ -2,11 +2,34 @@
 
 ProtectionOverheatInputFilter::ProtectionOverheatInputFilter()
 {
+  idMode_ = CCS_PROT_OTHER_OVERHEAT_INPUT_FILTER_MODE;
+  idPrevent_ = CCS_PROT_OTHER_OVERHEAT_INPUT_FILTER_PREVENT;
 
+  lastReasonRun_ = LastReasonRunApvOverheatInputFilter;
+  lastReasonStop_ = LastReasonStopOverheatInputFilter;
+
+  protReactEventId_ = OverheatInputFilterProtReactId;
+  protBlockedEventId_ = OverheatInputFilterProtBlockedId;
 }
 
 ProtectionOverheatInputFilter::~ProtectionOverheatInputFilter()
 {
 
+}
+
+bool ProtectionOverheatInputFilter::checkAlarm()
+{
+  if (parameters.get(CCS_FILTER_INPUT)) {   // Включен входной фильтр
+    if (0) {                                // TODO: Условие аварии пока не известно
+      protReactEventId_ = OverheatInputFilterProtReactId;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ProtectionOverheatInputFilter::checkPrevent()
+{
+  return alarm_;
 }
 
