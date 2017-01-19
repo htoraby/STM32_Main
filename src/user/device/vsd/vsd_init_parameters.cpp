cb62291cf7e299558ff27b85c72bd992406d7aca
Shilyaev <shilyaev.ev@novomet.ru>
2017-01-19 15:18:35 +0500

    fix: Скада СНГ. Запись/чтение серийного номера СУ/КСУ

--- src/user/device/vsd/vsd_init_parameters.cpp
+++ src/user/device/vsd/vsd_init_parameters.cpp
@@ -1978,7 +1978,7 @@ void Vsd::initParameters()
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].validity                         = VALIDITY_ERROR;
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].value.float_t                    = 100.0;
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].min                              = 0.0;
-  parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].max                              = 500.0;
+  parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].max                              = 1000.0;
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].def                              = 100.0;
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].discret                          = 1;
   parameters_[VSD_RESONANCE_REMOVE - VSD_BEGIN].code                             = 0;
