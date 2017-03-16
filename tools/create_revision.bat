d5500dd1c924bbe5fe6179a7a29aed7098818b2c
Shilyaev <shilyaev.ev@novomet.ru>
2017-03-16 15:21:22 +0500

    change: Утилита для создания файла с номером ревизии генерирует файл только если ревизия изменилась

--- tools/create_revision.bat
+++ tools/create_revision.bat
@@ -6,5 +6,13 @@ for /f "eol=; tokens=1,2,3,4" %%x in ('git -C ../ rev-list --count master') do (
   set VERSION_REV=%%x
 )
 
-set str=#define VCS_REVISION %VERSION_REV%
-echo %str% > %REVFILE%
+set STR=#define VCS_REVISION %VERSION_REV%
+set IS_FIND=1
+for /f "delims=" %%a in ('find /i "%STR%" ^< %REVFILE%') do (
+  set str2=%%a 
+  set IS_FIND=0
+)
+
+if %IS_FIND% equ 1 (
+  echo %STR%>%REVFILE%
+)
